071e97140facdcedfcfd35ee7e8e145e0bf1a840
PrasannaKumarChalla <kumar.challa7893@gmail.com>
2017-07-16 04:17:19 -0500

    Added basic stub for custom control

---
+++ RatingControl.swift
@@ -0,0 +1,20 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Prasanna challa on 7/16/17.
+//  Copyright © 2017 Prasanna challa. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
