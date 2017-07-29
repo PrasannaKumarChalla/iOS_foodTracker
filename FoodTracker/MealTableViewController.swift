986a64e16bff6130140a81aec656cc263f95aecd
PrasannaKumarChalla <kumar.challa7893@gmail.com>
2017-07-28 19:06:50 -0500

    Fixed table view sections.

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -35,21 +35,22 @@ class MealTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return meals.count
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return meals.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Table view cells are reused and should be dequeued using a cell identifier.
         let cellIdentifier = "MealTableViewCell"
-       
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell  else {
             fatalError("The dequeued cell is not an instance of MealTableViewCell.")
         }
+        
         // Fetches the appropriate meal for the data source layout.
         let meal = meals[indexPath.row]
         
@@ -58,9 +59,8 @@ class MealTableViewController: UITableViewController {
         cell.ratingControl.rating = meal.rating
         
         return cell
-    }
  
-
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
